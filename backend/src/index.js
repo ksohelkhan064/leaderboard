88ad29c05c9ac46ef1228aadae4ea3aabaea4a0f
ksohelkhan064 <ksohelkhan064@gmail.com>
2024-10-26 01:52:01 +0530

    modification in index file and frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(
   cors({
     // origin: "*", 
-    origin:"http://localhost:5173",
+    origin: process.env.URL,
     methods: "GET,POST,PUT,DELETE,PATCH", // Allowed methods
   })
 );
